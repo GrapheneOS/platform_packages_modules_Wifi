aae648fb622b01e6b715b4735aa22602f0081352
Jimmy Chen <jimmycmchen@google.com>
2022-08-10 15:51:24 +0530

    wifi: check availability of IMS over IWLAN for all MmTelCapabilities
    When IMS is registered over IWLAN, wifi turn off need to wait for
    IMS PDN teardown complete indication. Currently wifi turn off is
    delayed only when VOLTE (MmTelCapabilities.CAPABILITY_TYPE_VOICE),
    however, other MmTelCapabilities like below can also be registered
    without VOLTE:
     - MmTelCapabilities.CAPABILITY_TYPE_VIDEO
     - MmTelCapabilities.CAPABILITY_TYPE_UT
     - MmTelCapabilities.CAPABILITY_TYPE_SMS
     - MmTelCapabilities.CAPABILITY_TYPE_CALL_COMPOSER
    
    isAvailable() will needs all services available
    to return true. Each service should be checked
    separately.
    
    Bug: 242127459
    Test: atest FrameworksWifiTests
    Change-Id: I9927f296fd1e45b0ff67201d470ddf06b8b0ad30

--- service/java/com/android/server/wifi/ConcreteClientModeManager.java
+++ service/java/com/android/server/wifi/ConcreteClientModeManager.java
@@ -438,6 +438,28 @@ public class ConcreteClientModeManager implements ClientModeManager {
         }
     }
 
+    private boolean isAnyImsServiceOverWlanAvailable(int subId) {
+        ImsMmTelManager imsMmTelManager = ImsMmTelManager.createForSubscriptionId(subId);
+        try {
+            int[] possibleServiceOverWlan = new int[] {
+                MmTelFeature.MmTelCapabilities.CAPABILITY_TYPE_VOICE,
+                MmTelFeature.MmTelCapabilities.CAPABILITY_TYPE_VIDEO,
+                MmTelFeature.MmTelCapabilities.CAPABILITY_TYPE_UT,
+                MmTelFeature.MmTelCapabilities.CAPABILITY_TYPE_SMS,
+                MmTelFeature.MmTelCapabilities.CAPABILITY_TYPE_CALL_COMPOSER,
+            };
+            for (int i: possibleServiceOverWlan) {
+                if (imsMmTelManager.isAvailable(i,
+                        ImsRegistrationImplBase.REGISTRATION_TECH_IWLAN)) {
+                    return true;
+                }
+            }
+        } catch (RuntimeException ex) {
+            Log.e(TAG, "IMS Manager is not available.", ex);
+        }
+        return false;
+    }
+
     /**
      * Get deferring time before turning off WiFi.
      */
@@ -474,17 +496,9 @@ public class ConcreteClientModeManager implements ClientModeManager {
             return 0;
         }
 
-        ImsMmTelManager imsMmTelManager = ImsMmTelManager.createForSubscriptionId(subId);
-        // If no wifi calling, no delay
-        try {
-            if (!imsMmTelManager.isAvailable(
-                    MmTelFeature.MmTelCapabilities.CAPABILITY_TYPE_VOICE,
-                    ImsRegistrationImplBase.REGISTRATION_TECH_IWLAN)) {
-                Log.d(getTag(), "IMS not registered over IWLAN for subId: " + subId);
-                return 0;
-            }
-        } catch (RuntimeException ex) {
-            Log.e(TAG, "IMS Manager is not available.", ex);
+        // If no IMS service over WLAN, no delay
+        if (!isAnyImsServiceOverWlanAvailable(subId)) {
+            Log.d(getTag(), "IMS not registered over IWLAN for subId: " + subId);
             return 0;
         }
 
