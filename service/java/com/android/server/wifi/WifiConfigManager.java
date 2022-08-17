dfb8aa6c6a10f26cc003b1719640510d1f291e95
Pratyush <codelab@pratyush.dev>
2022-08-17 08:30:59 +0200

    add option of always randomizing MAC addresses
    Co-authored-by: renlord <me@renlord.com>

--- service/java/com/android/server/wifi/WifiConfigManager.java
+++ service/java/com/android/server/wifi/WifiConfigManager.java
@@ -467,6 +467,9 @@ public class WifiConfigManager {
             return true;
         }
 
+        if (config.macRandomizationSetting == WifiConfiguration.RANDOMIZATION_ALWAYS) {
+            return true;
+        }
         if (!isMacRandomizationSupported()
                 || config.macRandomizationSetting == WifiConfiguration.RANDOMIZATION_NONE) {
             return false;
@@ -621,7 +624,8 @@ public class WifiConfigManager {
     private MacAddress updateRandomizedMacIfNeeded(WifiConfiguration config) {
         boolean shouldUpdateMac = config.randomizedMacExpirationTimeMs
                 < mClock.getWallClockMillis() || mClock.getWallClockMillis()
-                - config.randomizedMacLastModifiedTimeMs >= NON_PERSISTENT_MAC_REFRESH_MS_MAX;
+                - config.randomizedMacLastModifiedTimeMs >= NON_PERSISTENT_MAC_REFRESH_MS_MAX ||
+                config.macRandomizationSetting == WifiConfiguration.RANDOMIZATION_ALWAYS;
         if (!shouldUpdateMac) {
             return config.getRandomizedMacAddress();
         }
