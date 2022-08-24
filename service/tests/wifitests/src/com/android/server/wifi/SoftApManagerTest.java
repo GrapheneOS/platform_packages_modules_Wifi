83f0e7618a08792e737f0062727def2877847b9f
Quang Luong <qal@google.com>
2022-08-24 16:43:58 +0000

    Add user rejection reason for SoftAP failure
    SoftAP may fail due to user rejection of the iface conflict confirmation
    dialog, so we should add this reason as a distinct failure code.
    
    Bug: 239364438
    Test: atest SoftApManagerTest
    Change-Id: I5ef5e966ac8fed526e990f5de32ded703123ab9c

--- service/tests/wifitests/src/com/android/server/wifi/SoftApManagerTest.java
+++ service/tests/wifitests/src/com/android/server/wifi/SoftApManagerTest.java
@@ -471,7 +471,7 @@ public class SoftApManagerTest extends WifiBaseTest {
         mSoftApManager = createSoftApManager(apConfig, TEST_COUNTRY_CODE, ROLE_SOFTAP_TETHERED);
 
         verify(mCallback).onStateChanged(WifiManager.WIFI_AP_STATE_FAILED,
-                WifiManager.SAP_START_FAILURE_GENERAL);
+                WifiManager.SAP_START_FAILURE_USER_REJECTED);
         verify(mListener).onStartFailure(mSoftApManager);
     }
 
