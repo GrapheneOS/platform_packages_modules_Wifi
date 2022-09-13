ead3d2664dde2609ba01b985fee3ec3b052e7272
Nate Jiang <qiangjiang@google.com>
2022-09-13 16:01:16 -0700

    [Aware] Release Aware interface when attach failure
    Bug: 246576350
    Test: atest com.android.server.wifi
    Merged-In: I1b4dc8dfbcc957ef1beb5def99881eba3f2bab1c
    Change-Id: I1b4dc8dfbcc957ef1beb5def99881eba3f2bab1c
    (cherry picked from commit 6019488f3ea379383d0bbe4f0453d44e0b05bc50)

--- service/tests/wifitests/src/com/android/server/wifi/aware/WifiAwareStateManagerTest.java
+++ service/tests/wifitests/src/com/android/server/wifi/aware/WifiAwareStateManagerTest.java
@@ -3404,6 +3404,44 @@ public class WifiAwareStateManagerTest extends WifiBaseTest {
         verifyNoMoreInteractions(mMockNative, mockCallback);
     }
 
+
+    /**
+     * Verify when attach aware failed, the aware interface will be released.
+     */
+    @Test
+    public void testAttachFailureAndReleaseAware() throws Exception {
+        final int clientId = 132;
+        final int uid = 1000;
+        final int pid = 2000;
+        final String callingPackage = "com.google.somePackage";
+        final String callingFeature = "com.google.someFeature";
+
+        ConfigRequest configRequest = new ConfigRequest.Builder().build();
+
+        ArgumentCaptor<Short> transactionId = ArgumentCaptor.forClass(Short.class);
+        IWifiAwareEventCallback mockCallback = mock(IWifiAwareEventCallback.class);
+        InOrder inOrder = inOrder(mMockNative, mockCallback, mMockNativeManager);
+
+        mDut.enableUsage();
+        mMockLooper.dispatchAll();
+
+        // (1) connect and succeed
+        mDut.connect(clientId, uid, pid, callingPackage, callingFeature, mockCallback,
+                configRequest, false, mExtras);
+        mMockLooper.dispatchAll();
+        inOrder.verify(mMockNativeManager).tryToGetAware(new WorkSource(uid, callingPackage));
+        inOrder.verify(mMockNative).enableAndConfigure(transactionId.capture(), eq(configRequest),
+                eq(false), eq(true), eq(true), eq(false), eq(false), eq(false), anyInt());
+        short transactionIdConfig = transactionId.getValue();
+        mDut.onConfigFailedResponse(transactionIdConfig, NanStatusType.INTERNAL_FAILURE);
+        mMockLooper.dispatchAll();
+        inOrder.verify(mMockNativeManager).releaseAware();
+        inOrder.verify(mockCallback).onConnectFail(NanStatusType.INTERNAL_FAILURE);
+
+
+        verifyNoMoreInteractions(mMockNative, mockCallback);
+    }
+
     /**
      * Validate that trying to update-subscribe on a publish session fails.
      */
