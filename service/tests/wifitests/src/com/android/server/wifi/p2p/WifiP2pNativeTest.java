d88de128aa3b4d9877640ce636251c77497cba24
Shuibing Dai <shuibing@google.com>
2022-09-26 17:23:56 -0700

    Add p2p metrics when setting up interface fails
    Bug: 248326756
    Test: atest com.android.server.wifi
    
    Change-Id: I771b8677250b9b671f818fbc0f9560357b2b3c6d

--- service/tests/wifitests/src/com/android/server/wifi/p2p/WifiP2pNativeTest.java
+++ service/tests/wifitests/src/com/android/server/wifi/p2p/WifiP2pNativeTest.java
@@ -50,6 +50,7 @@ import com.android.dx.mockito.inline.extended.ExtendedMockito;
 import com.android.server.wifi.HalDeviceManager;
 import com.android.server.wifi.PropertyService;
 import com.android.server.wifi.WifiBaseTest;
+import com.android.server.wifi.WifiMetrics;
 import com.android.server.wifi.WifiNative;
 import com.android.server.wifi.WifiVendorHal;
 
@@ -94,6 +95,7 @@ public class WifiP2pNativeTest extends WifiBaseTest {
 
     @Mock private WifiNl80211Manager mWifiCondManager;
     @Mock private WifiNative mWifiNative;
+    @Mock private WifiMetrics mWifiMetrics;
     @Mock private WifiVendorHal mWifiVendorHalMock;
     @Mock private SupplicantP2pIfaceHal mSupplicantP2pIfaceHalMock;
     @Mock private HalDeviceManager mHalDeviceManagerMock;
@@ -133,12 +135,8 @@ public class WifiP2pNativeTest extends WifiBaseTest {
         mWifiClientInterfaceNames.add("wlan0");
         mWifiClientInterfaceNames.add("wlan1");
 
-        mWifiP2pNative = new WifiP2pNative(
-                mWifiCondManager,
-                mWifiNative,
-                mWifiVendorHalMock,
-                mSupplicantP2pIfaceHalMock,
-                mHalDeviceManagerMock,
+        mWifiP2pNative = new WifiP2pNative(mWifiCondManager, mWifiNative, mWifiMetrics,
+                mWifiVendorHalMock, mSupplicantP2pIfaceHalMock, mHalDeviceManagerMock,
                 mPropertyServiceMock);
 
         when(mWifiNative.getClientInterfaceNames()).thenReturn(mWifiClientInterfaceNames);
@@ -227,6 +225,8 @@ public class WifiP2pNativeTest extends WifiBaseTest {
                 any(HalDeviceManager.InterfaceDestroyedListener.class),
                 eq(mHandlerMock), eq(mWorkSourceMock))).thenReturn(null);
 
+        mWifiP2pNative.setupInterface(mDestroyedListenerMock, mHandlerMock, mWorkSourceMock);
+        verify(mWifiMetrics).incrementNumSetupP2pInterfaceFailureDueToHal();
         assertEquals(
                 mWifiP2pNative.setupInterface(
                         mDestroyedListenerMock, mHandlerMock, mWorkSourceMock),
@@ -262,6 +262,8 @@ public class WifiP2pNativeTest extends WifiBaseTest {
         when(mSupplicantP2pIfaceHalMock.isInitializationStarted()).thenReturn(false);
         when(mSupplicantP2pIfaceHalMock.initialize()).thenReturn(false);
 
+        mWifiP2pNative.setupInterface(mDestroyedListenerMock, mHandlerMock, mWorkSourceMock);
+        verify(mWifiMetrics).incrementNumSetupP2pInterfaceFailureDueToSupplicant();
         assertEquals(
                 mWifiP2pNative.setupInterface(
                         mDestroyedListenerMock, mHandlerMock, mWorkSourceMock),
@@ -282,6 +284,8 @@ public class WifiP2pNativeTest extends WifiBaseTest {
         when(mSupplicantP2pIfaceHalMock.initialize()).thenReturn(true);
         when(mSupplicantP2pIfaceHalMock.isInitializationComplete()).thenReturn(false);
 
+        mWifiP2pNative.setupInterface(mDestroyedListenerMock, mHandlerMock, mWorkSourceMock);
+        verify(mWifiMetrics).incrementNumSetupP2pInterfaceFailureDueToSupplicant();
         assertEquals(
                 mWifiP2pNative.setupInterface(
                         mDestroyedListenerMock, mHandlerMock, mWorkSourceMock),
@@ -302,6 +306,8 @@ public class WifiP2pNativeTest extends WifiBaseTest {
         when(mSupplicantP2pIfaceHalMock.isInitializationComplete()).thenReturn(true);
         when(mSupplicantP2pIfaceHalMock.setupIface(eq(TEST_IFACE))).thenReturn(false);
 
+        mWifiP2pNative.setupInterface(mDestroyedListenerMock, mHandlerMock, mWorkSourceMock);
+        verify(mWifiMetrics).incrementNumSetupP2pInterfaceFailureDueToSupplicant();
         assertEquals(
                 mWifiP2pNative.setupInterface(
                         mDestroyedListenerMock, mHandlerMock, mWorkSourceMock),
