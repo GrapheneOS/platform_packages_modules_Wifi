507d8e19a535d612dd40d84958fb1a5d73e19249
Sunil Ravi <sunilravi@google.com>
2022-10-12 19:56:54 +0000

    Update Layer2 Information before start provisioning
    L2Key information needs to be updated to ipclient before
    starting the provisioning. This is needed for network stack
    to check the DHCP lease cache and trigger DHCP request instead
    of DHCP discover, to the network which was connected before.
    
    Bug: 251367593
    Test: atest com.android.server.wifi.ClientModeImplTest#testLayer2InformationUpdate
    Test: Tested connect-disconnect to same AP multiple times and confirmed
    from the sniffer logs that only initial connection trigger DHCP discover
    packet and subsequent connections are initiating DHCP by sending  DHCP request
    packets.
    
    Change-Id: Ib370c4c9f02f9b3b948023ac7ab4cb47d5b40dc7

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -2905,10 +2905,7 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
             mWifiInfo.clearCurrentSecurityType();
             mWifiInfo.resetMultiLinkInfo();
         }
-        // Update the L2 Information to IP Layer only after STA is authorized for data transfer.
-        if (state == SupplicantState.COMPLETED) {
-            updateLayer2Information();
-        }
+
         // SSID might have been updated, so call updateCapabilities
         updateCapabilities();
 
@@ -5200,6 +5197,7 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                     }
                     mWifiInfo.setNetworkKey(config.getNetworkKeyFromSecurityType(
                             mWifiInfo.getCurrentSecurityType()));
+                    updateLayer2Information();
                     updateCurrentConnectionInfo();
                     transitionTo(mL3ProvisioningState);
                     break;
@@ -5911,6 +5909,7 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                     mLastNetworkId = connectionInfo.networkId;
                     mWifiInfo.setNetworkId(mLastNetworkId);
                     mWifiInfo.setMacAddress(mWifiNative.getMacAddress(mInterfaceName));
+                    updateLayer2Information();
                     updateCurrentConnectionInfo();
                     if (!Objects.equals(mLastBssid, connectionInfo.bssid)) {
                         mLastBssid = connectionInfo.bssid;
@@ -6473,6 +6472,7 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
                         mLastBssid = connectionInfo.bssid;
                         mWifiInfo.setBSSID(mLastBssid);
                         mWifiInfo.setNetworkId(mLastNetworkId);
+                        updateLayer2Information();
                         sendNetworkChangeBroadcastWithCurrentState();
                         updateCurrentConnectionInfo();
                         // Successful framework roam! (probably)
