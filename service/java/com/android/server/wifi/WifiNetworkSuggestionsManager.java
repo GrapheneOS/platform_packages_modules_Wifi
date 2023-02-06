14501f7967c84eba25ee71112d7e4d202d897f0d
Nate(Qiang) Jiang <qiangjiang@google.com>
2023-02-06 19:41:39 +0000

    Revert "[DO NOT MERGE] wifi: remove certificates for network factory reset"
    This reverts commit 27f2e1542358c642c0160a8dbca1c4a8e142b7d9.
    
    Reason for revert: functional regression
    
    Change-Id: I77c2875f7ab8bd77bbe01410abb6d3558c0479ae
    (cherry picked from commit e7a384b2f89a4502ae288ffa4c0b5642c9acf935)
    Merged-In: I77c2875f7ab8bd77bbe01410abb6d3558c0479ae

--- service/java/com/android/server/wifi/WifiNetworkSuggestionsManager.java
+++ service/java/com/android/server/wifi/WifiNetworkSuggestionsManager.java
@@ -1352,7 +1352,7 @@ public class WifiNetworkSuggestionsManager {
             removeFromPassPointInfoMap(ewns);
         } else {
             if (ewns.wns.wifiConfiguration.isEnterprise()) {
-                mWifiKeyStore.removeKeys(ewns.wns.wifiConfiguration.enterpriseConfig, false);
+                mWifiKeyStore.removeKeys(ewns.wns.wifiConfiguration.enterpriseConfig);
             }
             removeFromScanResultMatchInfoMapAndRemoveRelatedScoreCard(ewns, true);
             mWifiConfigManager.removeConnectChoiceFromAllNetworks(ewns
