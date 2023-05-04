493f5b93f0930b9f079e95e759867e0d641f68a5
Quang Luong <qal@google.com>
2023-05-04 20:37:31 +0000

    Fix HDM NPE when storing static chip info
    Bug: 280461985
    Test: atest HalDeviceManagerTest
    Change-Id: I9a06148be53b00edccc42c0792c8686c15b2e93a

--- service/java/com/android/server/wifi/HalDeviceManager.java
+++ service/java/com/android/server/wifi/HalDeviceManager.java
@@ -1316,7 +1316,9 @@ public class HalDeviceManager {
         return staticChipInfos;
     }
 
-    private StaticChipInfo[] convertWifiChipInfoToStaticChipInfos(WifiChipInfo[] chipInfos) {
+    @NonNull
+    private StaticChipInfo[] convertWifiChipInfoToStaticChipInfos(
+            @NonNull WifiChipInfo[] chipInfos) {
         StaticChipInfo[] staticChipInfos = new StaticChipInfo[chipInfos.length];
         for (int i = 0; i < chipInfos.length; i++) {
             WifiChipInfo chipInfo = chipInfos[i];
@@ -2304,7 +2306,7 @@ public class HalDeviceManager {
                     WifiChipInfo[] wifiChipInfos = getAllChipInfo();
                     if (wifiChipInfos != null) {
                         mCachedStaticChipInfos =
-                                convertWifiChipInfoToStaticChipInfos(getAllChipInfo());
+                                convertWifiChipInfoToStaticChipInfos(wifiChipInfos);
                         saveStaticChipInfoToStore(mCachedStaticChipInfos);
                         mIsConcurrencyComboLoadedFromDriver = true;
                     } else {
