32c8120aafecf7385edc8cba169e8f43a1bed005
chao.meng <chao.meng@unisoc.com>
2023-11-01 13:44:05 +0800

    WifiShellCommand support to set verbose level
    Bug: 308717610
    Test: adb shell cmd wifi set-verbose-logging enabled -l <0 to 4>
    Change-Id: I8fb4513a5abffac3f996593f9f9948983faffa7c

--- framework/java/android/net/wifi/WifiManager.java
+++ framework/java/android/net/wifi/WifiManager.java
@@ -8243,7 +8243,9 @@ public class WifiManager {
     public void enableVerboseLogging(@VerboseLoggingLevel int verbose) {
         try {
             mService.enableVerboseLogging(verbose);
-            mVerboseLoggingEnabled = verbose == VERBOSE_LOGGING_LEVEL_ENABLED;
+            mVerboseLoggingEnabled =
+                    verbose == VERBOSE_LOGGING_LEVEL_ENABLED_SHOW_KEY
+                            || verbose == VERBOSE_LOGGING_LEVEL_ENABLED;
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
         }
