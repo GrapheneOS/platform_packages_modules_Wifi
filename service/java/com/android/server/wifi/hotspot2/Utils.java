a071d7e5e7a7d327af0da5547b725315ff339302
Nate Jiang <qiangjiang@google.com>
2023-11-07 17:23:27 -0800

    Correct and simplify the SSID + BSSID string generation
    Also use the mac string with colon for easy reading
    
    Bug: 295398783
    Test: atest com.android.server.wifi
    Change-Id: I8948ca7a3c4e289857398abc4eacc74a60e2d9ee

--- service/java/com/android/server/wifi/hotspot2/Utils.java
+++ service/java/com/android/server/wifi/hotspot2/Utils.java
@@ -1,6 +1,5 @@
 package com.android.server.wifi.hotspot2;
 
-import static com.android.server.wifi.hotspot2.anqp.Constants.BYTES_IN_EUI48;
 import static com.android.server.wifi.hotspot2.anqp.Constants.BYTE_MASK;
 import static com.android.server.wifi.hotspot2.anqp.Constants.NIBBLE_MASK;
 import static com.android.server.wifi.proto.WifiStatsLog.WIFI_CONNECTION_RESULT_REPORTED__PASSPOINT_ROAMING_TYPE__ROAMING_NONE;
@@ -79,20 +78,6 @@ public abstract class Utils {
             throw new IllegalArgumentException("Bad MAC address: '" + s + "'");
         }
         return mac;
-    }
-
-    /**
-     * Convert from mac address as long to simple string in hex code, same as "%012x".
-     * @param mac The Mac address as long value.
-     * @return String value of mac address.
-     */
-    public static String macToSimpleString(long mac) {
-        StringBuilder sb = new StringBuilder();
-        for (int n = BYTES_IN_EUI48 - 1; n >= 0; n--) {
-            long b = (mac >>> (n * Byte.SIZE)) & BYTE_MASK;
-            sb.append(b > 0xf ? Long.toHexString(b) : "0" + Long.toHexString(b));
-        }
-        return sb.toString();
     }
 
     /**
