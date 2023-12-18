a57ba0bd28481fc9008309f197f3878d4a4ec5cb
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 15:10:37 +0200

    add checks for WIFI_PRIVILEGED_ANDROID_AUTO permission

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -1032,6 +1032,11 @@ public class WifiServiceImpl extends BaseWifiService {
                 == PackageManager.PERMISSION_GRANTED;
     }
 
+    private boolean checkAndroidAutoPermission(int pid, int uid) {
+        return mContext.checkPermission(android.Manifest.permission.WIFI_PRIVILEGED_ANDROID_AUTO, pid, uid)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
     private boolean checkMainlineNetworkStackPermission(int pid, int uid) {
         return mContext.checkPermission(NetworkStack.PERMISSION_MAINLINE_NETWORK_STACK, pid, uid)
                 == PackageManager.PERMISSION_GRANTED;
@@ -1185,6 +1190,20 @@ public class WifiServiceImpl extends BaseWifiService {
                 || isPrivileged(pid, uid)
                 || mWifiPermissionsUtil.isAdmin(uid, packageName)
                 || mWifiPermissionsUtil.isSystem(packageName, uid)
+                /**
+                 * @see #disconnect
+                 * @see #reconnect
+                 * @see #reassociate
+                 * @see #getConfiguredNetworks
+                 * @see #addOrUpdateNetwork
+                 * @see #removeNetwork
+                 * @see #enableNetwork
+                 * @see #disableNetwork
+                 *
+                 * All of these operations are allowed for unprivileged callers that have
+                 * targetSdk < 29
+                 */
+                || checkAndroidAutoPermission(pid, uid)
                 // TODO(b/140540984): Remove this bypass.
                 || (mWifiPermissionsUtil.checkSystemAlertWindowPermission(uid, packageName)
                 && !isGuestUser());
@@ -1251,7 +1270,7 @@ public class WifiServiceImpl extends BaseWifiService {
         }
         int callingUid = Binder.getCallingUid();
         int callingPid = Binder.getCallingPid();
-        boolean isPrivileged = isPrivileged(callingPid, callingUid);
+        boolean isPrivileged = isPrivileged(callingPid, callingUid) || checkAndroidAutoPermission(callingPid, callingUid);
         boolean isThirdParty = !isPrivileged
                 && !isDeviceOrProfileOwner(callingUid, packageName)
                 && !mWifiPermissionsUtil.isSystem(packageName, callingUid);
