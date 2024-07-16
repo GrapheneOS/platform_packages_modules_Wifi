cabedc1a4f0d29cee8cc0b287802cdf241db3563
Nate Jiang <qiangjiang@google.com>
2024-07-16 13:34:23 -0700

    Fix the deadlock issue
    Reduce the locked code.
    
    Flag: EXEMPT bugfix
    Bug: 350897503
    Bug: 290822251
    Test: atest com.android.server.wifi
    Change-Id: I497b8a7db2a8ce0c78735c3805dbdf5e91fe8ca6

--- service/java/com/android/server/wifi/HalDeviceManager.java
+++ service/java/com/android/server/wifi/HalDeviceManager.java
@@ -67,7 +67,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -2492,6 +2491,7 @@ public class HalDeviceManager {
             return false;
         }
 
+        boolean success = false;
         synchronized (mLock) {
             WifiChip chip = getChip(iface);
             if (chip == null) {
@@ -2504,7 +2504,6 @@ public class HalDeviceManager {
                 return false;
             }
 
-            boolean success = false;
             switch (type) {
                 case WifiChip.IFACE_TYPE_STA:
                     mClientModeManagers.remove(name);
@@ -2523,20 +2522,20 @@ public class HalDeviceManager {
                     Log.wtf(TAG, "removeIfaceInternal: invalid type=" + type);
                     return false;
             }
+        }
 
-            // dispatch listeners no matter what status
-            dispatchDestroyedListeners(name, type);
-            if (validateRttController) {
-                // Try to update the RttController
-                updateRttControllerWhenInterfaceChanges();
-            }
+        // dispatch listeners no matter what status
+        dispatchDestroyedListeners(name, type);
+        if (validateRttController) {
+            // Try to update the RttController
+            updateRttControllerWhenInterfaceChanges();
+        }
 
-            if (success) {
-                return true;
-            } else {
-                Log.e(TAG, "IWifiChip.removeXxxIface failed, name=" + name + ", type=" + type);
-                return false;
-            }
+        if (success) {
+            return true;
+        } else {
+            Log.e(TAG, "IWifiChip.removeXxxIface failed, name=" + name + ", type=" + type);
+            return false;
         }
     }
 
@@ -2546,20 +2545,19 @@ public class HalDeviceManager {
     // onlyOnOtherThreads = false: call all listeners
     private void dispatchDestroyedListeners(String name, int type) {
         if (VDBG) Log.d(TAG, "dispatchDestroyedListeners: iface(name)=" + name);
+        InterfaceCacheEntry entry;
+        List<InterfaceDestroyedListenerProxy> triggerList;
         synchronized (mLock) {
-            InterfaceCacheEntry entry = mInterfaceInfoCache.remove(Pair.create(name, type));
+            entry = mInterfaceInfoCache.remove(Pair.create(name, type));
             if (entry == null) {
                 Log.e(TAG, "dispatchDestroyedListeners: no cache entry for iface(name)=" + name);
                 return;
             }
-
-            Iterator<InterfaceDestroyedListenerProxy> iterator =
-                    entry.destroyedListeners.iterator();
-            while (iterator.hasNext()) {
-                InterfaceDestroyedListenerProxy listener = iterator.next();
-                iterator.remove();
-                listener.action();
-            }
+            triggerList = new ArrayList<>(entry.destroyedListeners);
+            entry.destroyedListeners.clear();
+        }
+        for (InterfaceDestroyedListenerProxy listener : triggerList) {
+            listener.action();
         }
     }
 
@@ -2570,9 +2568,7 @@ public class HalDeviceManager {
         List<InterfaceDestroyedListenerProxy> triggerList = new ArrayList<>();
         synchronized (mLock) {
             for (InterfaceCacheEntry cacheEntry: mInterfaceInfoCache.values()) {
-                for (InterfaceDestroyedListenerProxy listener : cacheEntry.destroyedListeners) {
-                    triggerList.add(listener);
-                }
+                triggerList.addAll(cacheEntry.destroyedListeners);
                 cacheEntry.destroyedListeners.clear(); // for insurance
             }
             mInterfaceInfoCache.clear();
