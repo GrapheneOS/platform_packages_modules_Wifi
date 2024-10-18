0bddde3a06bd34106634008e15924c69a37200e1
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-18 11:52:00 +0300

    don't enable the "send DHCP hostname" option by default

--- service/java/com/android/server/wifi/WifiConfigurationUtil.java
+++ service/java/com/android/server/wifi/WifiConfigurationUtil.java
@@ -289,7 +289,7 @@ public class WifiConfigurationUtil {
     public static boolean hasSendDhcpHostnameEnabledChanged(WifiConfiguration existingConfig,
             WifiConfiguration newConfig) {
         if (existingConfig == null) {
-            return !newConfig.isSendDhcpHostnameEnabled();
+            return newConfig.isSendDhcpHostnameEnabled();
         }
         return newConfig.isSendDhcpHostnameEnabled() != existingConfig.isSendDhcpHostnameEnabled();
     }
