6bb4f5ce3cae0339e28cffea9ddae67acd20c5d8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-20 17:25:36 +0300

    gmscompat: use per-network MAC randomization in Android Auto by default

--- framework/java/android/net/wifi/WifiConfiguration.java
+++ framework/java/android/net/wifi/WifiConfiguration.java
@@ -3405,6 +3405,12 @@ public class WifiConfiguration implements Parcelable {
         mIpProvisioningTimedOut = false;
         mVendorData = Collections.emptyList();
         mIsAllowedToUpdateByOtherUsers = true;
+
+        if (android.app.compat.gms.GmsCompat.isAndroidAuto()) {
+            // Per-connection MAC randomization doesn't work with some cars, see
+            // https://github.com/GrapheneOS/os-issue-tracker/issues/4139
+            macRandomizationSetting = RANDOMIZATION_PERSISTENT;
+        }
     }
 
     /**
