189b8583d4b3737dc79b7fe057b2e08e3a650479
Nate Jiang <qiangjiang@google.com>
2025-12-08 21:11:38 -0800

    Also remove the SIM passpoint network
    Bug: 458518546
    Test: atest com.android.server.wifi
    Flag: EXEMPT BUGFIX
    Change-Id: I4ae0a0f514c6037f6eb829a1d1dc8abe0a525346

--- service/tests/wifitests/src/com/android/server/wifi/WifiConfigManagerTest.java
+++ service/tests/wifitests/src/com/android/server/wifi/WifiConfigManagerTest.java
@@ -5189,6 +5189,35 @@ public class WifiConfigManagerTest extends WifiBaseTest {
     }
 
     /**
+     * Verifies that the method resetSimNetworks removes passpoint networks.
+     */
+    @Test
+    public void testResetSimNetworksRemovesPasspoint() throws Exception {
+        when(mDataTelephonyManager.getSubscriberId()).thenReturn("3214561234567890");
+        when(mDataTelephonyManager.getSimApplicationState())
+                .thenReturn(TelephonyManager.SIM_STATE_LOADED);
+        when(mDataTelephonyManager.getSimOperator()).thenReturn("321456");
+        when(mDataTelephonyManager.getCarrierInfoForImsiEncryption(anyInt())).thenReturn(null);
+        List<SubscriptionInfo> subList = List.of(mock(SubscriptionInfo.class));
+        when(mSubscriptionManager.getCompleteActiveSubscriptionInfoList()).thenReturn(subList);
+        when(mSubscriptionManager.getActiveSubscriptionIdList())
+                .thenReturn(new int[]{DATA_SUBID});
+
+        WifiConfiguration passpointNetwork = WifiConfigurationTestUtil.createPasspointNetwork();
+        // Make it SIM based
+        passpointNetwork.enterpriseConfig.setEapMethod(WifiEnterpriseConfig.Eap.SIM);
+        passpointNetwork.enterpriseConfig.setPhase2Method(WifiEnterpriseConfig.Phase2.NONE);
+
+        verifyAddPasspointNetworkToWifiConfigManager(passpointNetwork);
+
+        // SIM was removed, resetting SIM Networks
+        mWifiConfigManager.resetSimNetworks();
+
+        // Verify passpoint network is removed
+        assertNull(mWifiConfigManager.getConfiguredNetwork(passpointNetwork.networkId));
+    }
+
+    /**
      * {@link WifiConfigManager#resetSimNetworks()} should reset all non-PEAP SIM networks, no
      * matter if {@link WifiCarrierInfoManager#getSimIdentity()} returns null or not.
      */
