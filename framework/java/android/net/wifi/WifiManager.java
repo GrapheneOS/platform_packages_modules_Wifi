df791e0aa720733f52aa0fc9fe6fa948a88d3541
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2026-01-31 03:53:14 -0800

    add NETWORK_SETUP_WIZARD permission to setScanAlwaysAvailable() allowlist
    This aligns with other network related methods and allows the setup wizard to call
    setScanAlwaysAvailable() without the more privileged NETWORK_SETTINGS permission.

--- framework/java/android/net/wifi/WifiManager.java
+++ framework/java/android/net/wifi/WifiManager.java
@@ -4845,7 +4845,9 @@ public class WifiManager {
      * @see #isScanAlwaysAvailable()
      */
     @SystemApi
-    @RequiresPermission(android.Manifest.permission.NETWORK_SETTINGS)
+    @RequiresPermission(anyOf = {
+            android.Manifest.permission.NETWORK_SETTINGS,
+            android.Manifest.permission.NETWORK_SETUP_WIZARD})
     public void setScanAlwaysAvailable(boolean isAvailable) {
         try {
             mService.setScanAlwaysAvailable(isAvailable, mContext.getOpPackageName());
